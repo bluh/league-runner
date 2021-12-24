fe4d338c7ad6938ec6bbc2e50b417591f6983a75
Joseph Faraci <joefaraci1@gmail.com>
2021-12-24 00:53:54 -0500

    Bundling updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
@@ -9,6 +10,19 @@ const apis = require('./api');
 
 const app = express();
 
+const production = (process.argv[2] === "production");
+
+// Use middleware
+app.use(helmet({
+  contentSecurityPolicy: {
+    useDefaults: true,
+    directives: {
+      "script-src": production 
+        ? ["'self'", "https://unpkg.com"] // Don't allow eval in prod
+        : ["'self'", "'unsafe-eval'", "https://unpkg.com"] // Allow in dev for speed
+    }
+  }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -42,13 +56,16 @@ const options = {
 
 const swaggerSpec = swaggerJSDoc(options);
 
-console.log(swaggerSpec);
-
 app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Use backup routing
+// Use static site routing
 app.use('/', express.static('public'), (_, res) => {
-  res.sendFile(path.resolve(__dirname, '../public/index.html'));
+  res.sendFile(path.resolve(__dirname, `../public/index_${production ? "prod" : "dev"}.html`));
 });
 
-app.listen(process.env.APP_PORT);
+const server = app.listen(process.env.APP_PORT);
+
+// Stop server peacefully
+process.on('SIGTERM', () => {
+  server.close();
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,51 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: "./src/views/index.js",
-  output: {
-    path: path.resolve(__dirname, "public/scripts"),
-    filename: "react.js",
-    publicPath: "scripts"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+module.exports = (env) => 
+  ({
+    mode: env.production ? "production" : "development",
+    entry: "./src/views/index.js",
+    output: {
+      path: path.resolve(__dirname, "public/scripts"),
+      filename: "react.js",
+      publicPath: "scripts"
+    },
+    devtool: env.production ? false : "eval-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
           }
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-        ],
-      },
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+          ],
+        },
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin()
     ]
-  }
-}
+  })
